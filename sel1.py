c840d5e134d2975513d045a922a25c1ac8b95398
Rohit Baweja <rohitbaweja@Rohits-MacBook-Pro.local>
2023-08-14 21:24:20 +0530

    discount formula

--- sel1.py
+++ sel1.py
@@ -10,3 +10,13 @@ print("Logs enabled")
 x = np.random.choice([3, 5, 7, 9], p=[0.1, 0.3, 0.6, 0.0], size=(1))
 
 print(x)
+
+shoes = {'name': 'Fancy Shoes ' , 'price':14900}
+
+def applydisc(product , discount):
+    price = int(product['price'] * (1.0 - discount))
+    assert 0 <= price <= product['price']
+    return price
+
+amount = applydisc(shoes,.30)
+print(amount)
